130cf825b95cccbad425bd475f474cf537016406
ducbinhnguyennd <ducbinhnguyennd@gmail.com>
2023-12-28 17:49:49 +0700

    upadte sửa sản phẩm

--- controllers/home.controller.js
+++ controllers/home.controller.js
@@ -1,5 +1,6 @@
-
 var myMD = require("../models/sanpham.model");
+var myMDBlog = require("../models/blog.model");
+
 
 
 
@@ -16,7 +17,7 @@ async function resizeImage(buffer, width, height) {
   const pixelRatio = originalSize / newSize;
   const step = Math.floor(Math.sqrt(pixelRatio));
   const newBuffer = Buffer.alloc(newSize * 4);
-  
+
   for (let i = 0; i < newSize; i++) {
     const srcIdx = i * step * 4;
     const destIdx = i * 4;
@@ -41,11 +42,13 @@ async function resizeImage(buffer, width, height) {
 
 exports.home = async (req, res, next) => {
   let list_TL = await myMD.spModel.find();
-  res.render("home/index.ejs", { listSP: list_TL });
+  let list_BL = await myMDBlog.blogModel.find();
+  res.render("home/index.ejs", { listSP: list_TL, listBL: list_BL });
 };
 exports.main = async (req, res, next) => {
   let list_TL = await myMD.spModel.find();
-  res.render("home/home.ejs", { listSP: list_TL });
+  let list_BL = await myMDBlog.blogModel.find();
+  res.render("home/home.ejs", { listSP: list_TL, listBL: list_BL });
 };
 exports.shop = async (req, res, next) => {
   let list_TL = await myMD.spModel.find();
@@ -61,14 +64,16 @@ exports.thanhtoan = async (req, res, next) => {
 };
 exports.searchByName = async (req, res, next) => {
   const searchQuery = req.query.search;
-  const regex = new RegExp(searchQuery, 'i'); 
+  const regex = new RegExp(searchQuery, 'i');
 
   try {
     const searchResults = await myMD.spModel.find({ name: regex });
-    res.render('home/home.ejs', { listSP: searchResults });
+    const searchResultsBlog = await myMDBlog.blogModel.find({ tieude_blog: regex });
+
+    res.render('home/home.ejs', { listSP: searchResults, listBL: searchResultsBlog });
   } catch (error) {
     console.error(error);
-    res.render('home/home.ejs', { listSP: [] }); 
+    res.render('home/home.ejs', { listSP: [], listBL: [] });
   }
 };
 
@@ -91,7 +96,7 @@ exports.add = async (req, res, next) => {
     } catch (error) {
       msg = error.message;
     }
-    
+
     objSP.name = req.body.name;
     objSP.noidung = req.body.noidung;
     objSP.price = req.body.price;
@@ -128,11 +133,163 @@ exports.add = async (req, res, next) => {
     }
   }
 
-  
+
 
   res.render("home/add.ejs", { msg: msg });
-};  
+};
 
+exports.addblog = async (req, res, next) => {
+  let msg = "";
+  console.log(req.body);
+  if (req.method == "POST") {
+    let objSP = new myMDBlog.blogModel();
+
+    try {
+      // Đọc buffer của ảnh từ req.file
+      const imageBuffer = req.file.buffer;
+
+      // Thực hiện điều chỉnh kích thước (ví dụ: giảm kích thước xuống 800x600)
+      const resizedBuffer = await resizeImage(imageBuffer, 600, 400);
+
+      // Chuyển đổi buffer thành base64
+      objSP.img_blog = resizedBuffer.toString('base64');
+    } catch (error) {
+      msg = error.message;
+    }
+
+    objSP.tieude_blog = req.body.tieude_blog;
+    objSP.noidung_blog = req.body.noidung_blog;
+
+
+    //ghi vào CSDL
+    try {
+      let new_sp = await objSP.save();
+      console.log(new_sp);
+      msg = "Lưu thành công";
+      res.redirect("/main");
+
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+
+
+  res.render("home/addblog.ejs", { msg: msg });
+};
+exports.addJsonBlog = async (req, res, next) => {
+  let msg = "";
+  console.log(req.body);
+  if (req.method == "POST") {
+    let objSP = new myMDBlog.blogModel();
+
+    try {
+      // Đọc buffer của ảnh từ req.file
+      const imageBuffer = req.file.buffer;
+
+      // Thực hiện điều chỉnh kích thước (ví dụ: giảm kích thước xuống 800x600)
+      const resizedBuffer = await resizeImage(imageBuffer, 600, 400);
+
+      // Chuyển đổi buffer thành base64
+      objSP.img_blog = resizedBuffer.toString('base64');
+    } catch (error) {
+      msg = error.message;
+    }
+
+    objSP.tieude_blog = req.body.tieude_blog;
+    objSP.noidung_blog = req.body.noidung_blog;
+
+
+    try {
+      let new_sp = await objSP.save();
+      console.log(new_sp);
+      msg = "Lưu thành công";
+    } catch (error) {
+      msg = "Error" + error.message();
+      console.log(error);
+    }
+  }
+  res.send({ message: msg });
+};
+exports.deleteBlog = async (req, res, next) => {
+  let idblog = req.params.idblog;
+  try {
+    await myMDBlog.blogModel.findByIdAndDelete(idblog);
+  } catch (error) { }
+  res.redirect("/main");
+
+};
+
+
+exports.deleteJsonBlog = async (req, res, next) => {
+  let idblog = req.params.idblog;
+  if (req.method === "DELETE") {
+    try {
+      await myMDBlog.blogModel.findByIdAndDelete({ _id: idblog });
+    } catch (error) { }
+  }
+  res.send({ message: 'Xóa thành công' });
+};
+exports.editBlog = async (req, res, next) => {
+  let msg = "";
+
+  // Load thông tin sản phẩm
+  let idblog = req.params.idblog;
+  let objSP = await myMDBlog.blogModel.findById(idblog);
+
+  if (req.method === "POST") {
+    try {
+      if (req.file) {
+        objSP.img = req.file.buffer.toString('base64');
+        console.log(msg);
+      }
+    } catch (error) {
+      msg = error.message;
+    }
+
+    objSP.tieude_blog = req.body.tieude_blog;
+    objSP.noidung_blog = req.body.noidung_blog;
+    try {
+      await objSP.save();
+      msg = "Cập nhật thành công";
+      res.redirect("/main");
+
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  res.render("home/editBlog.ejs", { msg: msg, objSP: objSP });
+};
+exports.editBlogJson = async (req, res, next) => {
+  let msg = "";
+
+  let idblog = req.params.idblog;
+  let objSP = await myMDBlog.blogModel.findById(idblog);
+
+  if (req.method == "PUT") {
+
+    objSP.tieude_blog = req.body.tieude_blog;
+    objSP.noidung_blog = req.body.noidung_blog;
+
+    try {
+      let new_sp = await objSP.save();
+      console.log(new_sp);
+      msg = "Sửa thành công";
+    } catch (error) {
+      msg = "Error" + error.message();
+      console.log(error);
+    }
+  }
+
+  res.send({ message: msg });
+};
+exports.chitietblog = async (req, res, next) => {
+  let idblog = req.params.idblog;
+  let objSP = await myMDBlog.blogModel.findById(idblog);
+  let listBL = await myMDBlog.blogModel.find();
+  res.render("home/chitietblog.ejs", { objSP: objSP, listBL: listBL });
+};
 
 //////////
 
@@ -142,8 +299,8 @@ exports.addJson = async (req, res, next) => {
   let objSP = new myMD.spModel();
 
   if (req.method == "POST") {
-    
-    objSP.img = req.body.img; 
+
+    objSP.img = req.body.img;
     objSP.name = req.body.name;
     objSP.noidung = req.body.noidung;
     objSP.price = req.body.price;
@@ -168,14 +325,14 @@ exports.addJson = async (req, res, next) => {
     objSP.tinhnagdacbiet = req.body.tinhnagdacbiet;
     objSP.loai = req.body.loai;
 
-      try {
-          let new_sp = await objSP.save();
-          console.log(new_sp);
-          msg = "Lưu thành công";
-      } catch (error) {
-          msg = "Error" + error.message();
-          console.log(error);
-      }
+    try {
+      let new_sp = await objSP.save();
+      console.log(new_sp);
+      msg = "Lưu thành công";
+    } catch (error) {
+      msg = "Error" + error.message();
+      console.log(error);
+    }
   }
   res.send({ message: msg });
 };
@@ -242,8 +399,8 @@ exports.editJson = async (req, res, next) => {
   let objSP = await myMD.spModel.findById(idsp);
 
   if (req.method == "PUT") {
-    
-    objSP.img = req.body.img; 
+
+    objSP.img = req.body.img;
     objSP.name = req.body.name;
     objSP.noidung = req.body.noidung;
     objSP.price = req.body.price;
@@ -268,14 +425,14 @@ exports.editJson = async (req, res, next) => {
     objSP.tinhnagdacbiet = req.body.tinhnagdacbiet;
     objSP.loai = req.body.loai;
 
-      try {
-          let new_sp = await objSP.save();
-          console.log(new_sp);
-          msg = "Sửa thành công";
-      } catch (error) {
-          msg = "Error" + error.message();
-          console.log(error);
-      }
+    try {
+      let new_sp = await objSP.save();
+      console.log(new_sp);
+      msg = "Sửa thành công";
+    } catch (error) {
+      msg = "Error" + error.message();
+      console.log(error);
+    }
   }
 
   res.send({ message: msg });
@@ -285,7 +442,7 @@ exports.deleteSP = async (req, res, next) => {
   let idsp = req.params.idsp;
   try {
     await myMD.spModel.findByIdAndDelete(idsp);
-  } catch (error) {}
+  } catch (error) { }
   res.redirect("/main");
 
 };
@@ -293,10 +450,10 @@ exports.deleteSP = async (req, res, next) => {
 
 exports.deleteJson = async (req, res, next) => {
   let idsp = req.params.idsp;
-  if(req.method === "DELETE"){
+  if (req.method === "DELETE") {
     try {
       await myMD.spModel.findByIdAndDelete({ _id: idsp });
-    } catch (error) {}
+    } catch (error) { }
   }
   res.send({ message: 'Xóa thành công' });
 };
@@ -304,6 +461,6 @@ exports.chitiet = async (req, res, next) => {
   let idsp = req.params.idsp;
   let objSP = await myMD.spModel.findById(idsp);
   let listSP = await myMD.spModel.find();
-  res.render("home/single-product.ejs", { objSP: objSP, listSP: listSP});
+  res.render("home/single-product.ejs", { objSP: objSP, listSP: listSP });
 };
- 
+
