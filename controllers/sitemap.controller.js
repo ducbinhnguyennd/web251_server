cb8544d5a569b7b21387f96295de7b5ce06ce78a
trafdual <traz08102003@gmail.com>
2024-01-05 13:53:35 +0700

    f

--- controllers/sitemap.controller.js
+++ controllers/sitemap.controller.js
@@ -4,7 +4,7 @@ const paths = ['/thanhtoan', '/shop', '/contact'];
 exports.sitemap = async (req, res, next)=>{
     try {
         const smStream = new SitemapStream({ hostname: 'https://www.9mobile.shop' });
-        const pipeline = smStream.pipe(createReadStream('../sitemap.xml')).pipe(res);
+        const pipeline = smStream.pipe(createReadStream('sitemap.xml')).pipe(res);
 
         // Thêm các đường dẫn vào sitemap
         paths.forEach(path => {
