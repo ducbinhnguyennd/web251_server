2421fff2e456317ebdcab59c8c54cf50d62cc54b
trafdual <traz08102003@gmail.com>
2024-01-05 13:51:28 +0700

    f

--- controllers/sitemap.controller.js
+++ controllers/sitemap.controller.js
@@ -1,15 +1,10 @@
 const { SitemapStream, streamToPromise } = require('sitemap');
-const { createWriteStream } = require('fs'); // Chú ý sự thay đổi ở đây
-
+const { createReadStream } = require('fs');
 const paths = ['/thanhtoan', '/shop', '/contact'];
-
-exports.sitemap = async (req, res, next) => {
+exports.sitemap = async (req, res, next)=>{
     try {
         const smStream = new SitemapStream({ hostname: 'https://www.9mobile.shop' });
-
-        // Tạo hoặc ghi đè tệp sitemap.xml
-        const writeStream = createWriteStream('sitemap.xml');
-        smStream.pipe(writeStream);
+        const pipeline = smStream.pipe(createReadStream('../sitemap.xml')).pipe(res);
 
         // Thêm các đường dẫn vào sitemap
         paths.forEach(path => {
@@ -18,16 +13,9 @@ exports.sitemap = async (req, res, next) => {
 
         // Kết thúc quá trình tạo sitemap
         smStream.end();
-
-        // Chờ hoàn thành ghi tệp
-        await streamToPromise(writeStream);
-
-        // Đọc và trả về nội dung của tệp sitemap.xml cho client
-        const readStream = createReadStream('sitemap.xml');
-        readStream.pipe(res);
-
+        await streamToPromise(pipeline);
     } catch (error) {
         console.error(error);
         res.status(500).end();
     }
-};
+}
